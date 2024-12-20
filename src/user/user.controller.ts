27af595992b0884acfd37f2a00705156fbe1b4ed
creativeSandeepKumar <sdp4209@gmail.com>
2024-12-20 10:16:29 +0530

    user dto applied

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,7 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query, ValidationPipe } from '@nestjs/common';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('user')
 export class UserController {
@@ -11,23 +13,31 @@ export class UserController {
     findAll(@Query("role") role?: 'INTERN' | 'ENGINEER' | 'ADMIN') { return this.userService.findAll(role) }
 
     @Get(":id")
-    findOne(@Param("id") id: string){
+    findOne(@Param("id", ParseIntPipe) id: number){
         return this.userService.findOne(+id)
     }
 
+    // @Post()
+    // create(@Body() user: {name: string, email: string, role: 'INTERN' | 'ENGINEER' | 'ADMIN'}){
+    //     return this.userService.create(user)
+    // }
     @Post()
-    create(@Body() user: {name: string, email: string, role: 'INTERN' | 'ENGINEER' | 'ADMIN'}){
-        return this.userService.create(user)
+    create(@Body() createUserDto: CreateUserDto){
+        return this.userService.create(createUserDto)
     }
 
+    // @Patch(':id')
+    // update(@Param('id') id: string, @Body() userUpdate: {name: string, email: string, role: 'INTERN' | 'ENGINEER' | 'ADMIN'}){
+    //     return this.userService.update(+id, userUpdate)
+    // }
     @Patch(':id')
-    update(@Param('id') id: string, @Body() userUpdate: {name: string, email: string, role: 'INTERN' | 'ENGINEER' | 'ADMIN'}){
-        return this.userService.update(+id, userUpdate)
+    update(@Param('id', ParseIntPipe) id: number, @Body(ValidationPipe) updateUserDto: UpdateUserDto){
+        return this.userService.update(+id, updateUserDto)
     }
 
 
     @Delete(':id')
-    delete(@Param('id') id: string){
+    delete(@Param('id', ParseIntPipe) id: string){
         return this.userService.delete(+id)
     }
 
